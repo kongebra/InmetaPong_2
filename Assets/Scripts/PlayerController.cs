a82f74fe44322bff11835a37756ec1fd22c3d133
Tord Stavik <tord.stavik@gmail.com>
2023-09-24 13:47:51 +0200

    fixed sensitivity and input from controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed;
 
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
+        Console.WriteLine($"Vertical speed: {verticalInput * speed * Time.deltaTime}");
         transform.Translate(0, verticalInput * speed * Time.deltaTime, 0);
     }
 }
